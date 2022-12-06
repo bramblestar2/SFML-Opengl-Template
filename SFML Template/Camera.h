153f58f7a418841f7fc9d9355704d7f79c82ac81
Jay Feather2000 <thegals4321@gmail.com>
2022-12-05 20:43:30 -0600

    a

--- SFML Template/Camera.h
+++ SFML Template/Camera.h
@@ -12,7 +12,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-enum class Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN};
+enum class Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN };
 
 class Camera
 {
