66ca427d3119beea0b7f62e88d98cdd433a799d9
Jay Feather2000 <thegals4321@gmail.com>
2022-12-05 19:48:33 -0600

    a

---
+++ SFML Template/Camera.cpp
@@ -0,0 +1,139 @@
+#include "Camera.h"
+
+Camera::Camera(const sf::Vector2f _Size, const sf::Vector3f _Pos)
+{
+	cameraSize = _Size;
+	cameraSpeed = 0.5f;
+
+	mouseSensitivity = 0.15;
+	yaw = 0;
+	pitch = 0;
+	firstMouse = true;
+
+	view = glm::mat4(1.f);
+
+	cameraPos = glm::vec3(_Pos.x, _Pos.y, _Pos.z);
+	cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+	cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+	cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+	cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	updateView();
+}
+
+Camera::~Camera()
+{
+
+}
+
+void Camera::enableDepth()
+{
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
+}
+
+void Camera::setViewDistance(const float _Near, const float _Far)
+{
+	viewDistance = sf::Vector2f(_Near, _Far);
+}
+
+void Camera::setCameraPosition(const sf::Vector3f _Position)
+{
+	cameraPos.x = _Position.x;
+	cameraPos.y = _Position.y;
+	cameraPos.z = _Position.z;
+}
+
+void Camera::move(const sf::Vector3f _Direction)
+{
+	cameraPos.x += _Direction.x;
+	cameraPos.y += _Direction.y;
+	cameraPos.z += _Direction.z;
+}
+
+void Camera::update(const double _DT)
+{
+	view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glMultMatrixf(glm::value_ptr(view));
+}
+
+void Camera::updateMouseMovement(sf::Window* window, const double _DT)
+{
+	float xpos = sf::Mouse::getPosition(*window).x;
+	float ypos = sf::Mouse::getPosition(*window).y;
+
+	if (firstMouse)
+	{
+		last.x = sf::Mouse::getPosition(*window).x;
+		last.y = sf::Mouse::getPosition(*window).y;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - last.x;
+	float yoffset = last.y - ypos;
+	last.x = xpos;
+	last.y = ypos;
+
+	float sensitivity = 0.15f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(direction);
+}
+
+void Camera::updateMovement(const double _DT)
+{
+	float camSpeed = cameraSpeed * (_DT * 100);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl))
+		camSpeed *= 2;
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Tab))
+		camSpeed /= 2;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		glm::vec3 temp = camSpeed * cameraFront;
+		//temp.y = 0;
+		cameraPos += temp;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		glm::vec3 temp = camSpeed * cameraFront;
+		//temp.y = 0;
+		cameraPos -= temp;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * camSpeed;
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * camSpeed;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+		cameraPos.y += camSpeed;
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+		cameraPos.y -= camSpeed;
+}
+
+void Camera::updateView()
+{
+	glViewport(0.0f, 0.0f, cameraSize.x, cameraSize.y);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	float ratio = (float)cameraSize.x / (float)cameraSize.y;
+	glFrustum(-ratio, ratio, -1.f, 1.f, 1.f, 500.f);
+}
---
+++ SFML Template/Camera.h
@@ -0,0 +1,73 @@
+#pragma once
+//OpenGL
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window/Window.hpp>
+//SFML
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
+#include <SFML/System/Vector3.hpp>
+#include <SFML/System/Vector2.hpp>
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+enum class Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN};
+
+class Camera
+{
+public:
+	Camera(const sf::Vector2f _Size = sf::Vector2f(), const sf::Vector3f _Cam_Position = sf::Vector3f());
+	~Camera();
+
+	void setClearDepth(const float _Depth);
+	void enableDepth();
+
+	float getYaw() const;
+	float getPitch() const;
+	float getMouseSensitivity() const;
+
+	void setViewDistance(const float _Near, const float _Far);
+	void setCameraSpeed(const float _Speed) { cameraSpeed = _Speed; }
+	void setCameraPosition(const sf::Vector3f _Position);
+	void setMouseSensitivity(const float _Sensitivity) { mouseSensitivity = _Sensitivity; }
+	void setYaw(const float _Yaw) { yaw = _Yaw; }
+	void setPitch(const float _Pitch) { pitch = _Pitch; }
+
+	void move(const float _X, const float _Y, const float _Z) { move(sf::Vector3f(_X, _Y, _Z)); }
+	void move(const sf::Vector3f _Direction);
+
+	void update(const double _DT);
+	void updateMouseMovement(sf::Window* window, const double _DT);
+	void updateMovement(const double _DT);
+
+private:
+	void updateView();
+
+	sf::Vector2f cameraSize;
+	//X > Near
+	//Y > Far
+	sf::Vector2f viewDistance;
+
+	//Camera
+	glm::mat4 view;
+	
+	glm::vec3 cameraPos;
+	glm::vec3 cameraTarget;
+	glm::vec3 cameraDirection;
+
+	glm::vec3 cameraFront;
+	glm::vec3 cameraUp;
+
+	float cameraSpeed;
+	//
+
+	//Mouse looking
+	sf::Vector2f last;
+
+	float mouseSensitivity;
+	float yaw, pitch;
+	bool firstMouse;
+	//
+};
+
